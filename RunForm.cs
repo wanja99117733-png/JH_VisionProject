9c0e7dd2e9621844d5f454e5f09d2fdfa54ce456
JinhyeongRyu <wanja99117733@gmail.com>
2025-12-11 15:44:43 +0900

    RunForm 생성밑 촬상 버튼 구현

---
+++ RunForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JH_VisionProject.Core;
+
+namespace JH_VisionProject
+{
+    public partial class RunForm : Form
+    {
+        public RunForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGrab_Click(object sender, EventArgs e)
+        {
+            Global.Inst.InspStage.Grab(0);
+        }
+    }
+}
